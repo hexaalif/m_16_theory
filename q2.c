8a5d07461ed0d5582e314ae8b0988f74c71afec2
hexaalif <hexaalif2020@gmail.com>
2023-05-04 23:02:28 +0600

    file edited

--- q2.c
+++ q2.c
@@ -2,6 +2,22 @@
 
 int main()
 {
-
+    int n;
+    scanf("%d", &n);
+    int s = n - 1, k = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= s; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= k; j++)
+        {
+            printf("%d", i);
+        }
+        s--;
+        k++;
+        printf("\n");
+    }
     return 0;
 }
