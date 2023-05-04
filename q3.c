81791ec849f8631a59018857e54872c9e6f3a95e
hexaalif <hexaalif2020@gmail.com>
2023-05-04 23:22:29 +0600

    file edited

--- q3.c
+++ q3.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
-int count_before_zero(int arr, int sz)
+int count_before_zero(int *arr, int sz)
 {
-}
-
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    int arr[n], count = 0;
-    int i = 0;
-    for (i = 0; i < n; i++)
+    int count = 0;
+    for (int i = 0; i < sz; i++)
     {
         scanf("%d", &arr[i]);
         if (arr[i] != 0)
@@ -22,6 +15,14 @@ int main()
             break;
         }
     }
-    printf("%d", count);
+    return count;
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    printf("%d", count_before_zero(arr, n));
     return 0;
 }
