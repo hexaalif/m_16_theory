d05efade650aa008357f720360c176b62288248b
hexaalif <hexaalif2020@gmail.com>
2023-05-05 00:02:20 +0600

    file edited

--- q5.c
+++ q5.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-
+    char s[100];
+    scanf("%s", s);
+    int n = strlen(s);
+    int i = 0, common = 0;
+    for (i = 0; i < n / 2; i++)
+    {
+        if (s[i] == s[n - i - 1])
+        {
+            common++;
+        }
+    }
+    printf("%d\n", i);
+    printf("%d\n", common);
+    if (common == i)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not Palindrome");
+    }
+    // printf("%s", s);
     return 0;
 }
